93479375477e9ca5645d9a6b2f2412032c026ee5
asaadjawed <asaadjawed70@gmail.com>
2020-10-31 01:10:56 +0500

    first commit

---
+++ public/app.js
@@ -0,0 +1,16 @@
+
+
+function onbulb(){
+   
+    var bulb = document.getElementById("bulb");
+    bulb.src = "Images/OnBulb.png";
+    
+}
+
+
+function offbulb(){
+   
+    var bulb = document.getElementById("bulb");
+    bulb.src = "Images/off1.png";
+    
+}
